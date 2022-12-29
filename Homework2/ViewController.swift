59ca91a744c7a5f4a56439e7c329d46f42b8d27a
Deniz Ata EŞ <85442526+denizataes@users.noreply.github.com>
2022-12-29 23:17:29 +0300

    Initial Commit

---
+++ Homework2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Homework2
+//
+//  Created by Deniz Ata Eş on 29.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
